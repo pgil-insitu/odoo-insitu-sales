3e017fbef9cb7b3d72a678f991a1cfa196990941
Pablo Gil <pablogil@insitusales.com>
2026-08-16 11:07:11 -0700

    Release Marketplace visual update for Odoo 19

--- insitu_sales_connector/__manifest__.py
+++ insitu_sales_connector/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "inSitu Sales Connector",
     "summary": "Launch the inSitu Sales external API integration for Odoo",
-    "version": "19.0.1.0.5",
+    "version": "19.0.1.0.6",
     "category": "Sales/Sales",
     "author": "inSitu Sales",
     "website": "https://www.insitusales.com",
